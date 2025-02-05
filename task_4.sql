06f43fb46e381c1b912c1359d2f509facb806f44
Migiro-Johans <Migironyogora@gmail.com>
2025-02-04 20:54:37 -0800

    This is the right phrase.

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,4 @@
-USE alx_book_store;
-SELECT book_id, title, author_id, price, publication_date FROM books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
